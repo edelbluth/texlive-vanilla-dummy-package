b1c41acc34356de28f55ee417fa465e120b4a711
Juergen Edelbluth <juergen@edelbluth.eu>
2014-08-20 18:30:19 +0200

    Buildscript

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case "$1" in
+	install)
+		(cd 2013/debian && make prereq)
+		(cd 2014/debian && make prereq)
+	;;
+	script)
+		(cd 2013/debian && make build && make install)
+		(cd 2014/debian && make build && make install)
+	;;
+esac
