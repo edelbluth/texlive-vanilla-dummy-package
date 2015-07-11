90373d3d47ac3b6228d22728a592e239a51c1c89
Juergen Edelbluth <edelbluth@users.noreply.github.com>
2015-07-11 10:49:35 +0200

    2015 version

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ case "$1" in
 	install)
 		(cd 2013/debian && make prereq)
 		(cd 2014/debian && make prereq)
+		(cd 2015/debian && make prereq)
 	;;
 	script)
 		(cd 2013/debian && make build && make install)
 		(cd 2014/debian && make build && make install)
+		(cd 2015/debian && make build && make install)
 	;;
 esac
